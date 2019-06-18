c66630ba0451e8e29922b5f880d1314fb995dbc2
Luke Kachelein <lkachele@ucsd.edu>
2019-06-18 13:35:34 +0200

    Create hph.m

---
+++ hph.m
@@ -0,0 +1,16 @@
+% C = hph(H,P)
+% 
+% Gives the first column (rest redundant) of H*P*H'.
+% Testing for N = 2000 and M = 200, This method is ~12 times faster than
+% multiplying the matrices (doing H*P*H') and extracting the first column.
+% 
+% IN:   H = NxM model ("regressor") matrix
+% IN:   P = MxM prior, must be diagonal
+% 
+% OUT:  C = Nx1, the first column of H*P*H'
+% 
+function C = hph(H,P)
+N = size(H,1);
+HH = H.*H(1,:);
+C = HH*diag(P);
+end
